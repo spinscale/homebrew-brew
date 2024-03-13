b5ddfec21e63dc3e3d227d417f5db673b4ba2ee2
Alexander Reelsen <alexander@reelsen.net>
2024-03-13 23:08:57 +0100

    Update znai.rb to 1.67

--- znai.rb
+++ znai.rb
@@ -1,9 +1,9 @@
 class Znai < Formula
   desc "Build functional, maintainable, beautiful User Guides with Znai. Instant pages navigation. Local search. Multiple integrations to work with Python, Java, OpenAPI, etc. Transform \"getting started\" sections into slideshow for your workshops. Enterprise Docs Hub."
   homepage "https://github.com/testingisdocumenting/znai"
-  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/znai/znai-dist/1.64/znai-dist-1.64-znai.zip"
-  sha256 "85be9001647a9d6f55bf2907666e64dff0945f8b84f9c132cc8751bcd7b4db21"
-  version "1.64"
+  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/znai/znai-dist/1.67/znai-dist-1.67-znai.zip"
+  sha256 "00b28bbceab7b4dee966e2750f42b987f1f896d13368139a7ebd8ec7c0a2d049"
+  version "1.67"
 
   depends_on "java"
 
